01033c63adfd18a1d4e5b1cbc17a28561984dcfe
Antonio Vega <a.vegaramirez76@gmail.com>
2019-02-27 15:30:09 +0900

    add VR Game

---
+++ VRWinterGame/Assets/Scripts/UI/ViewInGame/ViewInGame.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ViewInGame : MonoBehaviour
+{
+
+    public Image normalImage;
+    public Image downImage;
+    public Image UpImage;
+
+    public static ViewInGame sharedInstance;
+
+    void Awake()
+    {
+        sharedInstance = this;
+    }
+
+    void Start()
+    {
+        normalImage.enabled = true;
+        downImage.enabled = false;
+        UpImage.enabled = false;
+    }
+
+    public void StateNormal()
+    {
+        if (GameManager.sharedInstance.currentGameState == GameState.inTheGame)
+        {
+            normalImage.enabled = true;
+            downImage.enabled = false;
+            UpImage.enabled = false;
+        }
+    }
+
+    public void StateDown()
+    {
+        if (GameManager.sharedInstance.currentGameState == GameState.inTheGame)
+        {
+            normalImage.enabled = false;
+            downImage.enabled = true;
+            UpImage.enabled = false;
+        }
+    }
+
+    public void StateHigh()
+    {
+        if (GameManager.sharedInstance.currentGameState == GameState.inTheGame)
+        {
+            normalImage.enabled = false;
+            downImage.enabled = false;
+            UpImage.enabled = true;
+        }
+    }
+
+    public void ResetStateSignal()
+    {
+        normalImage.enabled = true;
+        downImage.enabled = false;
+        UpImage.enabled = false;
+    }
+
+}
