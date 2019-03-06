f449e7d0976528dc20c3eef45f73c9231d563da5
Antonio Vega <a.vegaramirez76@gmail.com>
2019-03-06 13:50:06 +0900

    Debug the reset of the Game, Add script for global enum of serialconfig

---
+++ VRWinterGame/Assets/Scripts/Global/ScenePlayerManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum SceneCurrentStage { CalibrationScene, GameScene }
+
+public class ScenePlayerManager : MonoBehaviour
+{
+    public static ScenePlayerManager sharedInstance;
+
+    public SceneCurrentStage currentScene;
+
+    public string[] GameVRScenes;
+
+    void Awake()
+    {
+        sharedInstance = this;
+    }
+
+    void Start()
+    {
+        currentScene = SceneCurrentStage.CalibrationScene;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("The scene was load");
+            SkiGameScene();
+        }
+    }
+
+    public void SquatScene()
+    {
+        CurrentSceneGameManager(SceneCurrentStage.CalibrationScene);
+    }
+
+    public void SkiGameScene()
+    {
+        CurrentSceneGameManager(SceneCurrentStage.GameScene);
+    }
+
+    void CurrentSceneGameManager(SceneCurrentStage _currentScene)
+    {
+        if (_currentScene == SceneCurrentStage.CalibrationScene)
+        {
+            SceneManager.LoadScene(GameVRScenes[0]);
+        }
+        else if (_currentScene == SceneCurrentStage.GameScene)
+        {
+            SceneManager.LoadScene(GameVRScenes[1]);
+        }
+    }
+}
---
+++ VRWinterGame/Assets/Scripts/SkiScene/Rotations/CameraTriggerRotations.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTriggerRotations : MonoBehaviour
+{
+    [HideInInspector]
+    public bool cameraRotationX = false, cameraRotationY = false, cameraRotationZ = false, cameraFieldView = false;
+
+    public static CameraTriggerRotations sharedInstance;
+
+    void Awake()
+    {
+        sharedInstance = this;
+    }
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("RotatePointOne"))
+        {
+            cameraRotationX = true;
+            cameraRotationZ = true;
+
+        }
+        else if (collider.gameObject.CompareTag("RotatePointFour"))
+        {
+            cameraRotationY = true;
+            cameraFieldView = true;
+        }
+    }
+
+    public void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("RotatePointTwo"))
+        {
+            cameraRotationZ = false;
+        }
+
+        if (collider.gameObject.CompareTag("RotatePointFour"))
+        {
+            cameraRotationY = false;
+        }
+    }
+
+    public void ResetCameraTriggerRotations()
+    {
+        cameraRotationX = false;
+        cameraRotationY = false;
+        cameraRotationZ = false;
+        cameraFieldView = false;
+    }
+}
