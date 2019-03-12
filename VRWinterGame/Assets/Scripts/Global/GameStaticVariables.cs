c36c35f5dcf78b1f16ec0efb9f8a0f9850a5360e
Antonio Vega <a.vegaramirez76@gmail.com>
2019-03-12 10:13:41 +0900

    Update GameStaticVariables.cs

--- VRWinterGame/Assets/Scripts/Global/GameStaticVariables.cs
+++ VRWinterGame/Assets/Scripts/Global/GameStaticVariables.cs
@@ -5,5 +5,5 @@ using UnityEngine;
 public class GameStaticVariables : MonoBehaviour
 {
     // This is the variable that we will define in the skill of the player and connect to the number of ramps and speed
-    static int UserSkillLevel = 1;
+    public static int UserSkillLevel = 1;
 }
